7ee6aff11b44c74038dd92778fcee21248549100
Robert E <robert.erez@gmail.com>
2019-11-03 19:05:18 +1000

    dsdfd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,19 @@
 #!/bin/sh -l
 
-echo "Hellos $1"
-time=$(date)
-echo ::set-output name=time::$time
+GITVERSION="$(dotnet GitVersion.dll /github/workspace)"
+
+echo $GITVERSION
+
+function GetComponent {
+	echo $GITVERSION | grep -oP "\""$1'":"?\K([^",]*)'
+}
+
+echo ::set-output name=GITVERSION_NuGetVersionV2::$(GetComponent NuGetVersionV2)
+echo ::set-output name=GITVERSION_Major::$(GetComponent Major)
+echo ::set-output name=GITVERSION_Minor::$(GetComponent Minor)
+echo ::set-output name=GITVERSION_Patch::$(GetComponent Patch)
+echo ::set-output name=GITVERSION_FullSemVer::$(GetComponent FullSemVer)
+echo ::set-output name=GITVERSION_InformationalVersion::$(GetComponent InformationalVersion)
+echo ::set-output name=GITVERSION_PreReleaseTag::$(GetComponent PreReleaseTag)
+echo ::set-output name=GITVERSION_PreReleaseLabel::$(GetComponent PreReleaseLabel)
+echo ::set-output name=GITVERSION_BuildMetaData::$(GetComponent BuildMetaData)
