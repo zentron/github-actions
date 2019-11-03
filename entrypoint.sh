7b555cb0c3292e1e9fa0bad05c39de04b5032f20
Robert E <robert.erez@gmail.com>
2019-11-03 20:55:33 +1000

    dsdfd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-
-ls ./github -a
-ls /github/home -a
-ls /github/workspace -a
-ls /github/workflow -a
-
 GITVERSION="$(dotnet /app/GitVersion.dll ./)"
-
-
 
 echo $GITVERSION
 
@@ -16,12 +8,12 @@ function GetComponent {
 	echo $GITVERSION | grep -oP "\""$1'":"?\K([^",]*)'
 }
 
-echo ::set-output name=GITVERSION_NuGetVersionV2::$(GetComponent NuGetVersionV2)
-echo ::set-output name=GITVERSION_Major::$(GetComponent Major)
-echo ::set-output name=GITVERSION_Minor::$(GetComponent Minor)
-echo ::set-output name=GITVERSION_Patch::$(GetComponent Patch)
-echo ::set-output name=GITVERSION_FullSemVer::$(GetComponent FullSemVer)
-echo ::set-output name=GITVERSION_InformationalVersion::$(GetComponent InformationalVersion)
-echo ::set-output name=GITVERSION_PreReleaseTag::$(GetComponent PreReleaseTag)
-echo ::set-output name=GITVERSION_PreReleaseLabel::$(GetComponent PreReleaseLabel)
-echo ::set-output name=GITVERSION_BuildMetaData::$(GetComponent BuildMetaData)
+echo ::set-env name=GITVERSION_NuGetVersionV2::$(GetComponent NuGetVersionV2)
+echo ::set-env name=GITVERSION_Major::$(GetComponent Major)
+echo ::set-env name=GITVERSION_Minor::$(GetComponent Minor)
+echo ::set-env name=GITVERSION_Patch::$(GetComponent Patch)
+echo ::set-env name=GITVERSION_FullSemVer::$(GetComponent FullSemVer)
+echo ::set-env name=GITVERSION_InformationalVersion::$(GetComponent InformationalVersion)
+echo ::set-env name=GITVERSION_PreReleaseTag::$(GetComponent PreReleaseTag)
+echo ::set-env name=GITVERSION_PreReleaseLabel::$(GetComponent PreReleaseLabel)
+echo ::set-env name=GITVERSION_BuildMetaData::$(GetComponent BuildMetaData)
